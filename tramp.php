9fdef4098eee1df41a5f5aadd8a9363ce86f4dc9
barce <jbarcelona@gmail.com>
2010-04-24 13:17:21 -0700

    added getAdminOptions

--- tramp.php
+++ tramp.php
@@ -11,15 +11,22 @@ Description: Tramp stands for traffic amplifier. This plug-in amplifies traffic 
 if (!class_exists("TrampPlugin")) {
 
   class TrampPlugin {
+    var $adminOptionsName = "TrampPluginAdminOptions";
     function __construct() {
       // construct a class
     }
 
 
+    /*
+     * addHeaderCode -- adds a comment to the header
+     */
     function addHeaderCode() { 
       echo '<!-- TrampPlugin Installed -->' . "\n";
     }
     
+    /*
+     * addContent -- adds an RFC822 date to the bottom of a post
+     */
     function addContent($content='') {
       $s_date = date(DATE_RFC822);
       $content .= "<p>$s_date</p>";
@@ -29,6 +36,25 @@ if (!class_exists("TrampPlugin")) {
     function authorUpperCase($author='') {
       return strtoupper($author);
     } 
+
+
+    function getAdminOptions() {
+      $trampAdminOptions = array('show_header' => 'true',
+        'add_content'=> 'true',
+        'comment_author' => 'true',
+        'content' => ''
+      );
+      $trampOptions = get_options($this->adminOptionsName);
+      if (!empty($trampOptions)) {
+        foreach($trampOptions as $key => $option)
+          $trampAdminOptions[$key] = $option;
+      }
+
+      // store options in WordPress database
+      update_option($this->adminOptionsName, $trampAdminOptions);
+      return $trampAdminOptions;
+
+    }
 
   } // End class TrampPlugin
 
